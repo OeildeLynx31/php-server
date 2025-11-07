1b949e6f1d3089509344296a4588b01415a21082
Oeildelynx31 <104771596+OeildeLynx31@users.noreply.github.com>
2025-11-07 16:47:28 +0100

    Refactor headers variable to use an array
    Changed headers variable from string to array.

--- api/mail.php
+++ api/mail.php
@@ -2,8 +2,7 @@
 $to = "oeildelynx@top7box.com";
 $subject = "My subject";
 $txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = []
 
 echo mail($to,$subject,$txt,$headers);
 ?>
