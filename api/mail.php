2dae33bac02aa1a543fb4f6ba5a6e4f608d02cd1
Oeildelynx31 <104771596+OeildeLynx31@users.noreply.github.com>
2025-11-07 13:35:11 +0100

    Change mail function to echo its result

--- api/mail.php
+++ api/mail.php
@@ -5,5 +5,5 @@ $txt = "Hello world!";
 $headers = "From: webmaster@example.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 
-mail($to,$subject,$txt,$headers);
+echo mail($to,$subject,$txt,$headers);
 ?>
